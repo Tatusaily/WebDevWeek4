e580b89a8a2bac48caca037e6b533dc2bd0c8f0e
Tatusaily <tatu.sailynoja@gmail.com>
2024-04-12 10:45:25 +0300

    assignment 4&5 5 needs work

---
+++ src/api/routes/user-router.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import {
+  getUser,
+  getUserById,
+  postUser,
+  putUser,
+  deleteUser,
+} from '../controllers/user-controller.js';
+import multer from 'multer';
+
+const userRouter = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E5);
+        const uniquePrefix = (req.body.name).replaceAll(/,|-|_|'/gi, '').toLowerCase();
+        let extension = file.originalname.split('.').pop(); // Or mimetype?
+        if (extension !== ('jpg' || 'jpeg' || 'png' || 'gif')) {
+        extension = 'jpg';
+        }
+    
+        const filename = `${uniquePrefix}-${uniqueSuffix}.${extension}`;
+        cb(null, filename);
+    },
+});
+
+const upload = multer({dest: 'uploads/'});
+
+userRouter.route('/')
+    .get(getUser)
+    .post(upload.single("file"), postUser);
+
+userRouter.route('/:id')
+    .get(getUserById)
+    .put(putUser)
+    .delete(deleteUser);
+
+export default userRouter;
