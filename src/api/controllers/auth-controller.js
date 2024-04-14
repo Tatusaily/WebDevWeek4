59c706659020cacb064d8a9745a4f3079b80f4ee
Tatu <tatu.sailynoja@gmail.com>
2024-04-14 18:58:05 +0300

    auth controller return token

--- src/api/controllers/auth-controller.js
+++ src/api/controllers/auth-controller.js
@@ -1,11 +1,22 @@
 import {authenticateUser} from '../models/user-model.js'; 
+import jwt from 'jsonwebtoken';
+
+
 const login = async (req, res) => {
     const user = await authenticateUser(req.body);
-    if (user) {
-        res.json(user);
-    } else {
+    if (!user) {
         res.sendStatus(401);
+        return;
     }
+    const userForToken = {
+        user_id: user.user_id,
+        username: user.username,
+        role: user.role,
+        name: user.name,
+        email: user.email,
+    };
+    const token = jwt.sign(userForToken, process.env.JWT_SECRET, {expiresIn: '1h'});
+    res.json({user: userForToken, token: token});
 };
 
 export {login};
